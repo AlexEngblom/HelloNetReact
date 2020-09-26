a1c992542a0a26d04ac49d1ee4bc79ae2b07da96
Alex <alex.engblomm@gmail.com>
2020-09-26 19:03:57 +0300

    Add webpack cleanup

--- HelloNetReact.UI/webpack.config.js
+++ HelloNetReact.UI/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/components/index.tsx",
@@ -37,5 +38,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "./src/components/index.css",
     }),
+    new CleanWebpackPlugin(),
   ],
 };

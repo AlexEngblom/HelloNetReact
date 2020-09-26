48eb196581ad5fdc8a3435d593bce6b736f0cdf9
Alex <alex.engblomm@gmail.com>
2020-09-26 21:01:17 +0300

    Change the frontend boilerplate

--- HelloNetReact.UI/src/components/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { HelloWorld } from "./App";
-
-ReactDOM.render(
-  <HelloWorld userName="Runner" lang="TypeScript" />,
-  document.getElementById("output")
-);
--- HelloNetReact.UI/webpack.config.js
+++ HelloNetReact.UI/webpack.config.js
@@ -1,43 +1,132 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path')
+const webpack = require('webpack')
+
+const TerserPlugin = require('terser-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DashboardPlugin = require('webpack-dashboard/plugin')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 module.exports = {
-  entry: "./src/components/index.tsx",
-  target: "web",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    overlay: true,
+    stats: 'minimal'
   },
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
-  },
+  devtool: isProduction ? 'source-map' : 'eval-source-map',
+  entry: './src/index',
+  mode: process.env.NODE_ENV,
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
-        loader: "awesome-typescript-loader",
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true
+            }
+          },
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              experimentalWatchApi: true
+            }
+          }
+        ]
       },
       {
-        enforce: "pre",
-        test: /\.js$/,
-        loader: "source-map-loader",
+        test: /\.jpg|png$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: isProduction ? '[name].[hash].[ext]' : '[name].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              mozjpeg: {
+                progressive: true,
+                quality: 80
+              },
+              optipng: {
+                optimizationLevel: 7
+              },
+              pngquant: {
+                enabled: false
+              }
+            }
+          }
+        ]
       },
       {
-        test: /\.css$/,
-        loader: "css-loader",
+        test: /\.svg/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          mimetype: 'image/svg+xml'
+        }
       },
-    ],
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+        options: {
+          interpolate: true,
+          minimize: isProduction
+        }
+      }
+    ]
   },
-  plugins: [
+  output: {
+    filename: isProduction ? '[name].[hash].js' : '[name].js',
+    path: path.resolve('./dist')
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        sourceMap: true,
+        extractComments: true
+      })
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+    modules: ['node_modules', 'src'],
+    plugins: [new TsconfigPathsPlugin()]
+  },
+  plugins: getPlugins()
+}
+
+function getPlugins() {
+  const plugins = [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "components", "index.html"),
+      template: './src/index.html'
     }),
-    new MiniCssExtractPlugin({
-      filename: "./src/components/index.css",
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': `"${process.env.NODE_ENV}"`
     }),
-    new CleanWebpackPlugin(),
-  ],
-};
+    new ForkTsCheckerWebpackPlugin(),
+    new CleanWebpackPlugin()
+  ]
+
+  if (!isProduction) {
+    plugins.push(new DashboardPlugin())
+  }
+
+  if (process.env.WEBPACK_BUNDLE_ANALYZER) {
+    plugins.push(new BundleAnalyzerPlugin())
+  }
+
+  return plugins
+}
